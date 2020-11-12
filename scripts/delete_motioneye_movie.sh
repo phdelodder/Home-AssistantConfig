b2c832e2ca4a898668591549d6b12e4a4564e87c
Philippe Delodder <philippe@delodder.be>
2020-11-12 21:54:32 +0100

    Move from zoneminder to montioneye with doods

---
+++ scripts/delete_motioneye_movie.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+timeout=5
+
+username="admin"
+password=""
+
+motion_camera_id=$1
+filename="$2.mp4"
+event="$3"
+
+echo $(date -u) "delete_motioneye_movie start up with paramaters: motion_camera_id -> ${motion_camera_id} || filename -> ${filename} || event -> ${event}" >> /config/scripts/delete_motioneye_movie.log
+
+uri="/movie/$motion_camera_id/delete/$filename?_username=$username"
+data="{}"
+signature=$(echo -n "POST:$uri:$data:$password" | sha1sum | cut -d ' ' -f 1)
+port="8765"
+
+curl -s -S -m $timeout -H "Content-Type: application/json" -X POST "http://cctv.fritz.box:$port$uri&_signature=$signature" -d "$data" >> /config/scripts/delete_motioneye_movie.log
+
