1201cfbf57b1474be519724ca7379e3455df783f
Philippe Delodder <philippe@delodder.be>
2020-12-04 09:41:01 +0100

    Each time script is called, also perform clean up

--- scripts/move_event_snapshots.sh
+++ scripts/move_event_snapshots.sh
@@ -6,3 +6,9 @@ event="$2"
 echo "$(date -u)" "move_event_snapshot start up with paramaters: motion_camera -> ${motion_camera} || event -> ${event} " >> /config/scripts/move_event_snapshot.log
 
 /bin/mv -v /config/www/security/"${motion_camera}"_"${event}"_*.jpg /config/www/security/"${motion_camera}"_motion/ >> /config/scripts/move_event_snapshot.log
+
+#clean unwanted snapshots after 14 days
+find /config/www/security/"${motion_camera}"*.jpg -type f -mtime +14 -exec rm -f {} \; 
+
+#clean wanted snapshots after 30 days
+find /config/www/security/"${motion_camera}"_motion/ -type f -mtime +30 -exec rm -f {} \; 
